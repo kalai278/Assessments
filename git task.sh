db460de02aa63064ce5348500e3d373b608b24bf
kalai278 <kalaiyarasi.gembeeran@agilisium.com>
2022-04-21 15:08:47 +0530

    success

---
+++ git task.sh
@@ -0,0 +1,34 @@
+ git clone "https://github.com/kalai278/Kalaii.git"
+ #changing the folder
+ cd kalaii
+#creating python file
+ % nano gk.py
+#adding into the file
+ git add .
+ git commit -m "Kalaiyarasi"
+#pushing the file in to git
+ git push
+Username for "https://github.com': kalai278"
+Password for "https://Kalaiyarasi@27" 
+ #adding changes to that python file
+  nano gk.py                
+ git add . 
+ git commit -m "this is gk"
+#pushing it to python
+ git push
+#creating a branch "develop"
+ git checkout -b develop
+ git branch
+#adding to that branch
+git add .
+git commit -m "hello"
+#pulling in to git
+ git pull
+ git checkout -b main   
+ git checkout main   
+ #pulling in to develop branch
+ git pull develop    
+ git checkout develop   
+ git push            
+ git push --set-upstream origin develop
+ git pull
